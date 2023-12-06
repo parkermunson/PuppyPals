0c1d62e981f68e93cca605d2d007963ce33ac7ac
parkermunson <149510295+parkermunson@users.noreply.github.com>
2023-12-05 21:30:33 -0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { puppyList } from './data.js'
+import './App.css'
+
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        console.log(featuredPup)
+        return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>;
+      })
+      }
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+
+}
+
+export default App
